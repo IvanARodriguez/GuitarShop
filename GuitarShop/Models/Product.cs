7586a041eb4921de5a2bd177121483f4d7cf0871
Ivan Rodriguez <ivancalvo91@gmail.com>
2022-02-18 14:14:48 -0600

    Add project files.

---
+++ GuitarShop/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace GuitarShop.Models
+{
+    public enum available
+    {
+        InStock,
+        OutOfStock,
+        OnOrder
+    }
+
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public available Availability { get; set; }
+        public string Slug => Name.Replace(' ', '-');
+    }
+}
